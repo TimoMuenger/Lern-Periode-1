125a2b29828332f6411665b0ba50a1a39fa6bce3
TimoMuenger <t.muenger.inf24@stud.bbbaden.ch>
2024-09-06 10:48:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace LA_Auftrag_Eingabefeld_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie heissisch du?");
+         string name = Console.ReadLine();
+
+
+            
+            Console.WriteLine("Kuhle Name");
+            Console.WriteLine("Wie alt bisch du?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Grüezi " + name);
+            Console.WriteLine("Du bisch also " + age + " Johr alt");
+
+            Console.WriteLine("Und wo wonsch du?");
+            string Wohnort = Console.ReadLine();
+            Console.WriteLine("Ah döt bini au mol gsie");
+
+            Console.WriteLine();
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
